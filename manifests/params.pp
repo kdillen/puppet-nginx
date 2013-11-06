0c873daba27f40bdad82cb4a8cb1c6f3f8e9893d
Kurt Dillen <kurt.dillen@super-visions.com>
2013-11-06 14:34:05 +0100

    * Modified nginx.conf.erb to include more variables. * Modified params.pp to be able to change configuration file settings.

--- manifests/params.pp
+++ manifests/params.pp
@@ -12,53 +12,85 @@
 #
 # Sample Usage:
 #
+# nx_events_use: One of [kqueue|rtsig|epoll|/dev/poll|select|poll|eventport]
+#                or false to use OS default
+#
 # This class file is not called directly
-class nginx::params {
-  $nx_temp_dir                = '/tmp'
-  $nx_run_dir                 = '/var/nginx'
+class nginx::params (
+  $nx_temp_dir                    = '/tmp',
+  $nx_run_dir                     = '/var/nginx',
 
-  $nx_conf_dir                = '/etc/nginx'
-  $nx_confd_purge             = false
-  $nx_worker_processes        = 1
-  $nx_worker_connections      = 1024
-  $nx_types_hash_max_size     = 1024
-  $nx_types_hash_bucket_size  = 512
-  $nx_names_hash_bucket_size  = 64
-  $nx_multi_accept            = off
-# One of [kqueue|rtsig|epoll|/dev/poll|select|poll|eventport]
-# or false to use OS default
-  $nx_events_use              = false
-  $nx_sendfile                = on
-  $nx_keepalive_timeout       = 65
-  $nx_tcp_nodelay             = on
-  $nx_gzip                    = on
-  $nx_server_tokens           = on
-  $nx_spdy                    = off
-  $nx_ssl_stapling            = off
+  $nx_conf_dir                    = '/etc/nginx',
+  $nx_confd_purge                 = false,
+  $nx_worker_processes            = 1,
+  $nx_worker_connections          = 1024,
+  $nx_types_hash_max_size         = 1024,
+  $nx_types_hash_bucket_size      = 512,
+  $nx_names_hash_bucket_size      = 64,
+  $nx_multi_accept                = off,
+  $nx_events_use                  = false,
+  $nx_sendfile                    = on,
+  $nx_keepalive_timeout           = '75 20',
+  $nx_tcp_nodelay                 = on,
+  $nx_tcp_nopush                  = on,
+  $nx_hash                        = on,
+  $nx_server_tokens               = on,
+  $nx_spdy                        = off,
+  $nx_ssl_stapling                = off,
+  $nx_ignore_invalid_headers      = on,
 
+  $nx_client_header_timeout       = '10m',
+  $nx_client_body_timeout         = '10m',
+  $nx_send_timeout                = '10m',
 
-  $nx_proxy_redirect          = off
-  $nx_proxy_set_header        = [
+  $nx_error_log_level             = 'info',
+  $nx_log_format                  = off,
+  $nx_log_format_name             = 'default',
+  $nx_log_description             = [
+    '\'$remote_addr - $remote_user [$time_local] \'',
+    '\'"$request" $status $bytes_sent \'',
+    '\'"$http_referer" "$http_user_agent" \'',
+    '\'"$gzip_ratio"\';',
+  ],
+
+  $nx_connection_pool_size        = 256,
+  $nx_client_header_buffer_size   = '1k',
+  $nx_large_client_header_buffers = '4 2k',
+  $nx_request_pool_size           = '4k',
+
+  $nx_gzip                        = on,
+  $nx_gzip_min_length             = 1100,
+  $nx_gzip_buffers                = '4 8k',
+  $nx_gzip_types                  = 'text/plain',
+
+  $nx_output_buffers              = '1 32k',
+  $nx_postpone_output             = 1460,
+  $nx_index                       = 'index.html',
+
+  $nx_proxy_redirect              = off,
+  $nx_proxy_set_header            = [
     'Host $host',
     'X-Real-IP $remote_addr',
     'X-Forwarded-For $proxy_add_x_forwarded_for',
-  ]
-  $nx_proxy_cache_path        = false
-  $nx_proxy_cache_levels      = 1
-  $nx_proxy_cache_keys_zone   = 'd2:100m'
-  $nx_proxy_cache_max_size    = '500m'
-  $nx_proxy_cache_inactive    = '20m'
+  ],
+  $nx_proxy_cache_path            = false,
+  $nx_proxy_cache_levels          = 1,
+  $nx_proxy_cache_keys_zone       = 'd2:100m',
+  $nx_proxy_cache_max_size        = '500m',
+  $nx_proxy_cache_inactive        = '20m',
 
+  $nx_client_body_buffer_size     = '128k',
+  $nx_client_max_body_size        = '10m',
+  $nx_proxy_connect_timeout       = '90',
+  $nx_proxy_send_timeout          = '90',
+  $nx_proxy_read_timeout          = '90',
+  $nx_proxy_buffers               = '32 4k',
+  $nx_proxy_http_version          = '1.0',
+  $nx_proxy_buffer_size           = '8k'
+)
+{
   $nx_client_body_temp_path   = "${nx_run_dir}/client_body_temp"
-  $nx_client_body_buffer_size = '128k'
-  $nx_client_max_body_size    = '10m'
   $nx_proxy_temp_path         = "${nx_run_dir}/proxy_temp"
-  $nx_proxy_connect_timeout   = '90'
-  $nx_proxy_send_timeout      = '90'
-  $nx_proxy_read_timeout      = '90'
-  $nx_proxy_buffers           = '32 4k'
-  $nx_proxy_http_version      = '1.0'
-  $nx_proxy_buffer_size       = '8k'
 
   $nx_logdir = $::kernel ? {
     /(?i-mx:linux)/ => '/var/log/nginx',
@@ -71,6 +103,10 @@ class nginx::params {
   $nx_daemon_user = $::operatingsystem ? {
     /(?i-mx:debian|ubuntu)/                                                    => 'www-data',
     /(?i-mx:fedora|rhel|redhat|centos|scientific|suse|opensuse|amazon|gentoo)/ => 'nginx',
+  }
+
+  if $::operatingsystem == 'gentoo' {
+    $nx_daemon_group = 'nginx'
   }
 
   # Service restart after Nginx 0.7.53 could also be just
